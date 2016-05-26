6947aba37c937413f197a019ef632f9ae102be85
John Swinbank <swinbank@princeton.edu>
2016-05-26 12:14:10 -0400

    Add role to link to JIRA.

--- ext/lsst.py
+++ ext/lsst.py
@@ -1,16 +1,21 @@
-# Convenient linking to Docushare documents.
+# Convenient linking to Docushare documents & JIRA tickets.
 
 from docutils import nodes
 
-def lsst_doc_role(name, rawtext, text, lineno, inliner, options=[], content={}):
-    node = nodes.raw(rawtext,
-                     "<a href=\"http://ls.st/%s-%s\">%s-%s</a>" %
-                     (name, text, name.upper(), text),
-                     format='html')
+def lsst_doc_role(name, rawtext, text, lineno, inliner,
+                  options=None, content=None):
+    node = nodes.reference(text="%s-%s" % (name.upper(), text),
+                           refuri="http://ls.st/%s-%s" % (name, text))
+    return [node], []
+
+def lsst_jira_role(name, rawtext, text, lineno, inliner,
+                   options=None, content=None):
+    node = nodes.reference(text=text,
+                           refuri="https://jira.lsstcorp.org/browse/"+text)
     return [node], []
 
 def setup(app):
     app.add_role('ldm', lsst_doc_role)
     app.add_role('lse', lsst_doc_role)
     app.add_role('lpm', lsst_doc_role)
-
+    app.add_role('jira', lsst_jira_role)
